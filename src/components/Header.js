0e21ec3456379429c45b342b40fa44df55097abb
vicentepaiva <vicentepaivadev@gmail.com>
2023-03-27 10:41:21 -0300

    second

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,9 @@ export function Header() {
     return (
         <header className="flex w-full  justify-center">
           <div className="flex justify-between max-w-6xl w-full p-4">
-            <h1>Possível logo aqui</h1>
+            
               <div className="flex gap-4">
-              {/* <Link to="/post" className="button rounded text-white bg-blue-600 px-4 py-2 hover:bg-blue-400">
-                Post
-            </Link> */}
+             
             <Link to="/usuario" className="transition duration-500 ease-in-out button rounded text-white bg-red-600 px-4 py-2 hover:bg-red-400">
                 Usuário
             </Link>
